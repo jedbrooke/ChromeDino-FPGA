7ed0311a1dc8caad7f6b83892274a8d71a8a2267
Eldon Ngo <40747060+eldon3141@users.noreply.github.com>
2020-01-31 11:06:26 -0800

    Added buffer for "Nicer UART Output"

--- lab2/src/tb/tb.v
+++ lab2/src/tb/tb.v
@@ -1,128 +1,77 @@
 `timescale 1ns / 1ps
 
-module tb;
+module model_uart(/*AUTOARG*/
+   // Outputs
+   TX,
+   // Inputs
+   RX
+   );
 
-   reg [7:0] sw;
-   reg       clk;
-   reg       btnS;
-   reg       btnR;
+   output TX;
+   input  RX;
+
+   parameter baud    = 115200;
+   parameter bittime = 1000000000/baud;
+   parameter name    = "UART0";
    
-   integer   i;
-   
-   /*AUTOWIRE*/
-   // Beginning of automatic wires (for undeclared instantiated-module outputs)
-   wire                 RsRx;                   // From model_uart0_ of model_uart.v
-   wire                 RsTx;                   // From uut_ of nexys3.v
-   wire [7:0]           led;                    // From uut_ of nexys3.v
-   // End of automatics
+   reg [7:0] rxData;
+   reg [31:0] buffer;
+   event     evBit;
+   event     evByte;
+   event     evTxBit;
+   event     evTxByte;
+   reg       TX;
 
    initial
      begin
-        //$shm_open  ("dump", , ,1);
-        //$shm_probe (tb, "ASTF");
-
-        clk = 0;
-        btnR = 1;
-        btnS = 0;
-        #1000 btnR = 0;
-        #1500000;
-        
-        tskRunPUSH(0,4);
-        tskRunPUSH(0,0);
-        tskRunPUSH(1,3);
-        tskRunMULT(0,1,2);
-        tskRunADD(2,0,3);
-        tskRunSEND(0);
-        tskRunSEND(1);
-        tskRunSEND(2);
-        tskRunSEND(3);
-        
-        #1000;        
-        $finish;
+        TX = 1'b1;
+     end
+   
+   always @ (negedge RX)
+     begin
+        rxData[7:0] = 8'h0;
+        #(0.5*bittime);
+        repeat (8)
+          begin
+             #bittime ->evBit;
+             //rxData[7:0] = {rxData[6:0],RX};
+             rxData[7:0] = {RX,rxData[7:1]};
+          end
+        ->evByte;
+        //$display ("%d %s Received byte %02x (%s)", $stime, name, rxData, rxData);
      end
 
-   always #5 clk = ~clk;
+  //Once we receive a byte, trigger this always block
+  always @(evByte) 
+    begin  
+      if rxData == "\r" //Display will convert buffer to HEX and append newline before outputting 
+          $display ("%d %s Received byte %02x (%s)", $stime, name, buffer, buffer);
+      	else //Left shift by 8 bits and add received data to last 8 bits
+          buffer[31:0] = {buffer[23:0],rxData[7:0]};
+    end 
+  
+   task tskRxData;
+      output [7:0] data;
+      begin
+         @(evByte);
+         data = rxData;
+      end
+   endtask // for
+      
+   task tskTxData;
+      input [7:0] data;
+      reg [9:0]   tmp;
+      integer     i;
+      begin
+         tmp = {1'b1, data[7:0], 1'b0};
+         for (i=0;i<10;i=i+1)
+           begin
+              TX = tmp[i];
+              #bittime;
+              ->evTxBit;
+           end
+         ->evTxByte;
+      end
+   endtask // tskTxData
    
-   model_uart model_uart0_ (// Outputs
-                            .TX                  (RsRx),
-                            // Inputs
-                            .RX                  (RsTx)
-                            /*AUTOINST*/);
-
-   defparam model_uart0_.name = "UART0";
-   defparam model_uart0_.baud = 1000000;
-   
-   
-   nexys3 uut_ (/*AUTOINST*/
-                // Outputs
-                .RsTx                   (RsTx),
-                .led                    (led[7:0]),
-                // Inputs
-                .RsRx                   (RsRx),
-                .sw                     (sw[7:0]),
-                .btnS                   (btnS),
-                .btnR                   (btnR),
-                .clk                    (clk));
-
-   task tskRunInst;
-      input [7:0] inst;
-      begin
-         $display ("%d ... Running instruction %08b", $stime, inst);
-         sw = inst;
-         #1500000 btnS = 1;
-         #3000000 btnS = 0;
-      end
-   endtask //
-
-   task tskRunPUSH;
-      input [1:0] ra;
-      input [3:0] immd;
-      reg [7:0]   inst;
-      begin
-         inst = {2'b00, ra[1:0], immd[3:0]};
-         tskRunInst(inst);
-      end
-   endtask //
-
-   task tskRunSEND;
-      input [1:0] ra;
-      reg [7:0]   inst;
-      begin
-         inst = {2'b11, ra[1:0], 4'h0};
-         tskRunInst(inst);
-      end
-   endtask //
-
-   task tskRunADD;
-      input [1:0] ra;
-      input [1:0] rb;
-      input [1:0] rc;
-      reg [7:0]   inst;
-      begin
-         inst = {2'b01, ra[1:0], rb[1:0], rc[1:0]};
-         tskRunInst(inst);
-      end
-   endtask //
-
-   task tskRunMULT;
-      input [1:0] ra;
-      input [1:0] rb;
-      input [1:0] rc;
-      reg [7:0]   inst;
-      begin
-         inst = {2'b10, ra[1:0], rb[1:0], rc[1:0]};
-         tskRunInst(inst);
-      end
-   endtask //
-
-   always @ (posedge clk)
-     if (uut_.inst_vld)
-       $display("%d ... instruction %08b executed", $stime, uut_.inst_wd);
-
-   always @ (led)
-     $display("%d ... led output changed to %08b", $stime, led);
-   
-endmodule // tb
-// Local Variables:
-// verilog-library-flags:("-y ../src/")
-// End:
+endmodule // model_uart
