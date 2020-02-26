6baa6088e5047cffc8fa36b0d28b54e418cb738f
Jasper Edbrooke <jasperredrock@gmail.com>
2020-02-26 11:41:56 -0800

    implement cactus

--- lab4/nexys3-vga-test/rectangle.v
+++ lab4/nexys3-vga-test/rectangle.v
@@ -3,15 +3,20 @@
 // Learn more at https://timetoexplore.net/blog/arty-fpga-vga-verilog-01
 
 `default_nettype none
+`include "parameters.v"
+
+endinterface
 
 module square #(
-    H_SIZE=80,      // half square width (for ease of co-ordinate calculations)
-    IX=320,         // initial horizontal position of square centre
-    IY=240,         // initial vertical position of square centre
+    WIDTH=40,       // half rect width (for ease of co-ordinate calculations)
+    HEIGHT=60,      // half rect height 
+    IX=D_WIDTH,     // initial horizontal position of square centre
+    IY=FLOOR_HEIGHT,// initial vertical position of square centre
     IX_DIR=0,       // initial horizontal direction: 1 is right, 0 is left
     IY_DIR=0,       // initial vertical direction: 1 is down, 0 is up
-    D_WIDTH=640,    // width of display
-    D_HEIGHT=480    // height of display
+    TYPE=0          // 0 is cactus =, 1 is bird
+    IX_VEL=1,
+    IY_VEL=0,
     )
     (
     input wire i_clk,         // base clock
@@ -29,10 +34,10 @@ module square #(
     reg x_dir = IX_DIR;  // horizontal animation direction
     reg y_dir = IY_DIR;  // vertical animation direction
 
-    assign o_x1 = x - H_SIZE;  // left: centre minus half horizontal size
-    assign o_x2 = x + H_SIZE;  // right
-    assign o_y1 = y - H_SIZE;  // top
-    assign o_y2 = y + H_SIZE;  // bottom
+    assign o_x1 = x - WIDTH;  // left: centre minus half horizontal size
+    assign o_x2 = x + WIDTH;  // right
+    assign o_y1 = y - HEIGHT;  // top
+    assign o_y2 = y + HEIGHT;  // bottom
 
     always @ (posedge i_clk)
     begin
@@ -45,17 +50,18 @@ module square #(
         end
         if (i_animate && i_ani_stb)
         begin
-            x <= (x_dir) ? x + 1 : x - 1;  // move left if positive x_dir
-            y <= (y_dir) ? y + 1 : y - 1;  // move down if positive y_dir
+            x <= (x_dir) ? x + IX_VEL : x - IX_VEL;  // move left if positive x_dir
+            y <= (y_dir) ? y + IY_VEL : y - IY_VEL;  // move down if positive y_dir
 
             if (x <= H_SIZE + 1)  // edge of square is at left of screen
-                x_dir <= 1;  // change direction to right
-            if (x >= (D_WIDTH - H_SIZE - 1))  // edge of square at right
-                x_dir <= 0;  // change direction to left          
+                x <= D_WIDTH + WIDTH;  // move back to the right of the screen
+            /*if (x >= (D_WIDTH - H_SIZE - 1))  // edge of square at right
+                x_dir <= 0;  // change direction to left         
             if (y <= H_SIZE + 1)  // edge of square at top of screen
                 y_dir <= 1;  // change direction to down
             if (y >= (D_HEIGHT - H_SIZE - 1))  // edge of square at bottom
-                y_dir <= 0;  // change direction to up              
+                y_dir <= 0;  // change direction to up     
+            */         
         end
     end
 endmodule
--- lab4/nexys3-vga-test/top.v
+++ lab4/nexys3-vga-test/top.v
@@ -10,26 +10,18 @@ module top(
     output wire VGA_HS_O,       // horizontal sync output
     output wire VGA_VS_O,       // vertical sync output
     output wire [2:0] VGA_R_O,
-	 output wire [2:0] VGA_G_O,
-	 output wire [1:0] VGA_B_O
+    output wire [2:0] VGA_G_O,
+	output wire [1:0] VGA_B_O
     );
-	 
-	 /*
-	 wire [3:0] VGA_R;    // 4-bit VGA red output
-    wire [3:0] VGA_G;    // 4-bit VGA green output
-    wire [3:0] VGA_B;     // 4-bit VGA blue output
-	 
-	 assign VGA_R_O = VGA_R[3:1];
-	 assign VGA_G_O = VGA_R[3:1];
-	 assign VGA_B_O = VGA_R[3:2];
 
- */ 
     // wire rst = ~RST_BTN;    // reset is active low on Arty & Nexys Video
     wire rst = RST_BTN;  // reset is active high on Basys3 (BTNC)
 
     wire [9:0] x;  // current pixel x position: 10-bit value: 0-1023
     wire [8:0] y;  // current pixel y position:  9-bit value: 0-511
     wire animate;  // high when we're ready to animate at end of drawing
+
+    reg px_color;
 
     // generate a 25 MHz pixel strobe
     reg [15:0] cnt = 0;
@@ -48,32 +40,34 @@ module top(
         .o_animate(animate)
     );
 
-    wire sq_a, sq_b, sq_c;
-    wire [11:0] sq_a_x1, sq_a_x2, sq_a_y1, sq_a_y2;  // 12-bit values: 0-4095 
+    wire catc_1, sq_b, sq_c;
+    wire [11:0] catc_1_x1, catc_1_x2, catc_1_y1, catc_1_y2;  // 12-bit values: 0-4095 
     wire [11:0] sq_b_x1, sq_b_x2, sq_b_y1, sq_b_y2;
     wire [11:0] sq_c_x1, sq_c_x2, sq_c_y1, sq_c_y2;
 
-    square #(.IX(160), .IY(120), .H_SIZE(60), .IX_DIR(0)) cactus (
+    rectangle cactus (
         .i_clk(CLK), 
         .i_ani_stb(pix_stb),
         .i_rst(rst),
         .i_animate(animate),
-        .o_x1(sq_a_x1),
-        .o_x2(sq_a_x2),
-        .o_y1(sq_a_y1),
-        .o_y2(sq_a_y2)
+        .o_x1(catc_1_x1),
+        .o_x2(catc_1_x2),
+        .o_y1(catc_1_y1),
+        .o_y2(catc_1_y2)
     );
 
 
 
-    assign sq_a = ((x > sq_a_x1) & (y > sq_a_y1) &
-        (x < sq_a_x2) & (y < sq_a_y2)) ? 1 : 0;
-    assign sq_b = ((x > sq_b_x1) & (y > sq_b_y1) &
+    assign catc_1 = ((x > catc_1_x1) & (y > catc_1_y1) &
+        (x < catc_1_x2) & (y < catc_1_y2)) ? 1 : 0;
+    /*assign sq_b = ((x > sq_b_x1) & (y > sq_b_y1) &
         (x < sq_b_x2) & (y < sq_b_y2)) ? 1 : 0;
     assign sq_c = ((x > sq_c_x1) & (y > sq_c_y1) &
-        (x < sq_c_x2) & (y < sq_c_y2)) ? 1 : 0;
+        (x < sq_c_x2) & (y < sq_c_y2)) ? 1 : 0;*/
 
-    assign VGA_R_O = {3{sq_a}};  // square a is red
-    assign VGA_G_O = {3{sq_b}};//sq_b;  // square b is green
-    assign VGA_B_O = {2{sq_c}};//sq_c;  // square c is blue
+    assign px_color = (catc_1) ? CACTUS_COLOR : 8'b0;
+
+    assign VGA_R_O = px_color[7:5];
+    assign VGA_G_O = px_color[4:2];
+    assign VGA_B_O = px_color[1:0];
 endmodule
