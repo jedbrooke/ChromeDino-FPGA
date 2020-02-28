57c2445cae8242c1eec6aa755123398f8726a948
Jasper Edbrooke <jasperredrock@gmail.com>
2020-02-27 18:05:03 -0800

    oh 2/27

--- lab4/nexys3-vga-test/randomValue.v
+++ lab4/nexys3-vga-test/randomValue.v
@@ -28,7 +28,7 @@ module MUX(i_a,i_b,i_sel,o_bit);
 		if(i_sel == 0) 
 			tempBit = i_a; 
 		else 
-			tempBit = i_B; 
+			tempBit = i_b; 
 	end
 
 	assign o_bit = tempBit; 
@@ -37,23 +37,29 @@ endmodule
 module randomValue(i_clk,i_rst,i_seed,i_load,o_randomBit); 
 	input i_clk; 
 	input i_rst; 
-	input i_seed; 
+	input[3:0] i_seed; 
 	input i_load; 
-	reg tempBit; 
+	wire tempBit; 
 	output o_randomBit; 
 
-	wire [3:0] current_state; 
-	wire [3:0] next_state; 
+	//wire [3:0] current_state; 
+	reg [3:0] next_state = 0; 
 
-	Dff Dff[3:0] (next_state,i_clk,i_rst,current_state); 
+	/*Dff Dff[3:0] (next_state,i_clk,i_rst,current_state); 
 	MUX MUX[3:0] (i_load,i_seed,current_state,{next_state[2],
 											   next_state[1], 
 											   next_state[0],
-											   tempBit});
-	always@(next_state[2],next_state[3])
-	begin
-		tempBit <= next_state[2]^next_state[3];
-	end 
+											   tempBit});*/
+	always @(posedge i_clk) begin
+		if(i_rst) begin
+			 next_state = 4'b0;
+		end else begin
+			 next_state[0] <= (i_load) ? o_randomBit : i_seed[0];
+			 next_state[1] <= (i_load) ? next_state[0] : i_seed[1];
+			 next_state[2] <= (i_load) ? next_state[1] : i_seed[2];
+			 next_state[3] <= (i_load) ? next_state[2] : i_seed[3];
+	   end
+	end
 
-	assign o_randomBit = tempBit; 
+	assign o_randomBit = next_state[2]^next_state[3]; ; 
 endmodule 
