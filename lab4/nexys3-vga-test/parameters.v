1b65b8e4d86a27a0e1865c8ae9b29bda325c7bc9
Jasper Edbrooke <jasperredrock@gmail.com>
2020-02-27 18:03:54 -0800

    oh 2/27

--- lab4/nexys3-vga-test/parameters.v
+++ lab4/nexys3-vga-test/parameters.v
@@ -2,10 +2,16 @@
 parameter D_WIDTH = 640;
 parameter D_HEIGHT = 480;
 //colors
-parameter CACTUS_COLOR = 8'b00111000;
-parameter BIRD_COLOR = 8'b01101111;
+parameter CACTUS_COLOR = 8'b00110100;
+parameter BIRD_COLOR = 8'b11011011;
+parameter FLOOR_COLOR = 8'b11111111;
+parameter BG_COLOR = 8'b0;
 //positions
-parameter FLOOR_HEIGHT = 400;
+parameter FLOOR_HEIGHT = 300;
 parameter BIRD_HEIGHT = 120;
+parameter FLOOR_POSX = 320;
+parameter FLOOR_POSY = 90;
 //game data
-parameter NUM_OBSTACLES = 2;
+parameter NUM_BIRDS = 1;
+parameter NUM_CACTI = 1;
+parameter OBSTACLE_VEL = 5;
