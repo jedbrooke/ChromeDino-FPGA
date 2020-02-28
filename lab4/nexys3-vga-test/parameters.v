3ed7f605777b26a0d6eef10fd5107e85c527ab47
Jasper Edbrooke <jasperredrock@gmail.com>
2020-02-27 18:15:37 -0800

    implement dino

--- lab4/nexys3-vga-test/parameters.v
+++ lab4/nexys3-vga-test/parameters.v
@@ -7,5 +7,12 @@ parameter BIRD_COLOR = 8'b01101111;
 //positions
 parameter FLOOR_HEIGHT = 400;
 parameter BIRD_HEIGHT = 120;
+//dino data
+parameter DINO_X = 80;
+parameter DINO_HEIGHT = 30;
+parameter DINO_DUCK_HEIGHT = 20;
+parameter DINO_WIDTH = 20;
+parameter JUMP_STRENGTH = 15;
+parameter DINO_GRAVITY = 10; //positive is down
 //game data
 parameter NUM_OBSTACLES = 2;
