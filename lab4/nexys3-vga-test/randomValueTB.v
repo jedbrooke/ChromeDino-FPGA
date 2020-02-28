5bc6e5d318cef9e320e075c82bedeac9c4cd79fa
Eldon Ngo <40747060+eldon3141@users.noreply.github.com>
2020-02-28 10:24:16 -0800

    Updated randomValue TB

--- lab4/nexys3-vga-test/randomValueTB.v
+++ lab4/nexys3-vga-test/randomValueTB.v
@@ -1,31 +1,26 @@
-`timescale 1ns/100ps
+`timescale 1ns / 100ps
 
-module randomValueTB; 
-	reg clk;
-	reg rst;
-	reg [3:0] seed; 
-	reg load; 
-	wire q; 
-	randomValue rV(q,clk,rst,seed,load); 
+module randomValueTB();
+reg clk_tb; 
+reg rst_tb; 
+wire[3:0] out_tb; 
 
-	initial 
-		begin
-			clk = 0; 
-			load = 0; 
-			seed = 0; 
-			rst = 0; 
-			#10 rst = 1; 
-			#300 rst = 0;
-			#1200 $finish;
-		end 
-
-	always 
-	#50 clk = ~clk;
-	initial begin
-		#100 seed = 4'b0001;
-		load = 1; 
-		#100 load = 0;
-	end 
+initial 
+begin 
+	clk_tb = 0; 
+	rst_tb = 1; 
+	#15; 
 	
-endmodule  
+	rst_tb = 0; 
+	#200; 
+end 
+
+always
+begin 
+	#5
+	clk_tb = ~clk_tb; 
+end 
+
+randomValue rV(out_tb,clk_tb,rst_tb); 
+endmodule 
 
