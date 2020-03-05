f273ce070289ca3a07f2b0d5500e33bd25de895f
Jasper Edbrooke <jasperredrock@gmail.com>
2020-03-04 18:14:42 -0800

    fix dino height

--- lab4/src/dinosaur.v
+++ lab4/src/dinosaur.v
@@ -28,36 +28,31 @@ module dinosaur (
     assign o_y1 = y - height;  // top
     assign o_y2 = y + height;  // bottom
 
-    always @ (posedge i_clk)
-    begin
-        if (i_rst) // on reset return to starting position
-        begin
+    always @ (posedge i_clk)begin
+        if (i_rst) begin // on reset return to starting position        
             x <= DINO_X;
             y <= FLOOR_HEIGHT - DINO_HEIGHT;
             y_vel <= 0;
-				is_jumping <= 0;
-        end else if (i_animate && i_ani_stb)
-        begin
-				if(is_jumping) begin
+			is_jumping <= 0;
+        end else if (i_animate && i_ani_stb) begin
+    		if(is_jumping) begin
                 y_vel <= y_vel + DINO_GRAVITY;
-					 y <= y + y_vel;		
-					 if(y > FLOOR_HEIGHT - height) begin
-						y <= FLOOR_HEIGHT - height;
-						y_vel <= 1'b0;
-						is_jumping <= 1'b0;
-					end
+        		y <= y + y_vel;		
+        		if(y > FLOOR_HEIGHT - height) begin
+        			y <= FLOOR_HEIGHT - height;
+        			y_vel <= 1'b0;
+        			is_jumping <= 1'b0;
+        		end
             end else if(i_jump) begin
-					is_jumping <= 1'b1;
-					y_vel <= -DINO_JUMP_STRENGTH;
-					y <= y - DINO_JUMP_STRENGTH;
-				end
-				
-				
-				
+				is_jumping <= 1'b1;
+				y_vel <= -DINO_JUMP_STRENGTH;
+				y <= y - DINO_JUMP_STRENGTH;
+			end
+            
             if(i_duck) begin
                 height <= DINO_DUCK_HEIGHT;
 					 if (y == FLOOR_HEIGHT - DINO_HEIGHT)
-						y <= y + (DINO_HEIGHT - DINO_DUCK_HEIGHT);
+						y <= y + ((DINO_HEIGHT - DINO_DUCK_HEIGHT) / 2);
             end else begin
                 height <= DINO_HEIGHT;
             end
