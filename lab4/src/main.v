e9ea60506df2edbb623f7b5fb105b82e7feb2bbc
Eldon Ngo <40747060+eldon3141@users.noreply.github.com>
2020-03-11 00:19:19 -0700

    Commented main.v

--- lab4/src/main.v
+++ lab4/src/main.v
@@ -19,31 +19,32 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module main(
-	  input wire i_clk,             // board clock: 100 MHz on Arty/Basys3/Nexys
-      input wire RST_BTN,           // reset button
-      input wire dino_jump,
-      input wire dino_duck,
-		input wire super_secret_switch,		//super secret switch
-		input wire super_secret_switch2,
-	  output wire VGA_HS_O,         // horizontal sync output
-      output wire VGA_VS_O,         // vertical sync output
-      output wire [2:0] VGA_R_O,
-      output wire [2:0] VGA_G_O,
-      output wire [1:0] VGA_B_O,
-	  output wire [6:0] seg,
-	  output wire [3:0] an
+      input wire i_clk,			// Board Clock: 100 MHz on Arty/Basys3/Nexys
+      input wire RST_BTN,		// Reset Button
+      input wire dino_jump,		// Jump Button 
+      input wire dino_duck,		// Duck Button 
+      input wire super_secret_switch,   // super secret switch
+      input wire super_secret_switch2,	// super secret switch 2
+      output wire VGA_HS_O,             // Horizontal Sync Output
+      output wire VGA_VS_O,             // vertical Sync Output
+      output wire [2:0] VGA_R_O,     // Red Output
+      output wire [2:0] VGA_G_O,      // Green Output
+      output wire [1:0] VGA_B_O,      // Blue Output
+      output wire [6:0] seg,          // 7-Segment Output
+      output wire [3:0] an            // 4-bit Anode Output
     );
 	 
 `include "parameters.v"
 
-	wire collided;
-	wire [15:0] nums;
-	wire [1:0] state;
-	wire score_clk;
-	wire dp_clk;
-	wire blink_clk;
-	wire fast_score_clk;
+	wire collided; //State of whether dino has collided with an obstacle 
+	wire [15:0] nums; //This will keep track of our score
+	wire [1:0] state; //The game will have three possible states 
+	wire score_clk; //Score Clock
+	wire dp_clk;	//Display Clock
+	wire blink_clk; //Blink Clock for when we're in dead state 
+	wire fast_score_clk; //Fast Score Clock for sss2 
 	 
+	//This will manage the state transitions 
 	gamestate gs (
 		.clk(i_clk),
 		.rst(RST_BTN),
@@ -55,6 +56,8 @@ module main(
 		.o_state(state)
 	);
 	
+	//[RENAME] 
+	//Based on user inputs and gamestate, the appropriate frames will be drawn
 	top top (
 		.i_clk(i_clk),
 		.RST_BTN(RST_BTN),
@@ -70,7 +73,7 @@ module main(
 		.collision(collided)
 	);
 	
-	//clk divider
+	//This produces the four clocks we need
 	clk_div clkdiv (
 		.clk(i_clk),
 		.rst(RST_BTN),
@@ -80,6 +83,7 @@ module main(
 		.fast_score_clk(fast_score_clk)
 	);
 	
+	//This handles the actual output of the 7-segment display for score
 	gpu gpu(
 		.dp_clk(dp_clk),
 		.blink_clk(blink_clk),
